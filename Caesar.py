34577f638216525283abaeff8ef1d7cfb161f90a
BuildTools <unconfigured@null.spigotmc.org>
2019-08-21 09:55:22 -0600

    created caesar.py

---
+++ Caesar.py
@@ -0,0 +1,20 @@
+# Remember you can only use the websites listed below: Good Luck!
+# Allowed Websites:
+# https://docs.python.org/3/
+# https://python-reference.readthedocs.io/en/latest/docs/str/ASCII.html
+# https://en.wikipedia.org/wiki/Caesar_cipher
+
+import curses
+
+key  = input('Key: ') # Get key for ceaser cypher
+if ascii.isalnum(key) == False #  Check to see if key is a number
+    keycheck = False
+    while keycheck = False:
+        print('Key is not a numeric value please try again')
+        key = input('Key: ')
+        if ascii.isalnum(key) == True
+            keycheck = True
+        else 
+            keycheck = False
+text = input('Text to be encrypted: ')
+
